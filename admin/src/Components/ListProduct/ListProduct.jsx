9c6c449598d64aee4700be9c7e9170fc40e35c31
Kiran333-git <mkirankumar511@gmail.com>
2025-09-14 02:14:02 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
 
     const [allproducts,setAllProducts]=useState([])
     const fetchInfo=async ()=>{
-        await fetch('http://localhost:4000/allproducts')
+        await fetch('https://e-commerce-backend-a7n4.onrender.com/allproducts')
         .then((res)=>res.json())
         .then((data)=>{setAllProducts(data)})
     }
@@ -14,7 +14,7 @@ const ListProduct = () => {
         fetchInfo()
     },[])
     const remove_product=async (id)=>{
-        await fetch('http://localhost:4000/removeproduct',{
+        await fetch('https://e-commerce-backend-a7n4.onrender.com/removeproduct',{
             method:'POST',
             headers:{
                 Accept:"application/json",
